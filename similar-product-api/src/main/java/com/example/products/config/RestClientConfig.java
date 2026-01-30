237e72f39d9443b600171bd48a96dd95448d27c1
jtristante <84666847+jtristante@users.noreply.github.com>
2026-01-30 02:43:53 +0100

    feat: Create RestTemplate configuration       - Configure RestTemplate bean       - Set timeouts (connect, read)

--- similar-product-api/src/main/java/com/example/products/config/RestClientConfig.java
+++ similar-product-api/src/main/java/com/example/products/config/RestClientConfig.java
@@ -2,6 +2,8 @@ package com.example.products.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +11,14 @@ public class RestClientConfig {
 
 	@Bean
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
+		return new RestTemplate(clientHttpRequestFactory());
+	}
+
+	@Bean
+	public ClientHttpRequestFactory clientHttpRequestFactory() {
+		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+		factory.setConnectTimeout(3000);
+		factory.setReadTimeout(5000);
+		return factory;
 	}
 }
